d16754700338d3472a4d8f126fb60ecb11c1ca99
dts <dts@esoft.com>
2024-11-27 09:38:24 +0000

    pos embs in model

--- models.py
+++ models.py
@@ -9,6 +9,39 @@ from timm.models.vision_transformer import Block as MHSABlock
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    if embed_dim % 2 != 0:
+        raise ValueError("embed_dim must be divisible by 2")
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = torch.concatenate([emb_h, emb_w], axis=1)  # (H*W, D)
+    return emb
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position pos: a list of positions to be encoded: size (M,) out: (M, D)
+    """
+    if embed_dim % 2 != 0:
+        raise ValueError("embed_dim must be divisible by 2")
+
+    omega = torch.arange(embed_dim // 2, dtype=torch.float)
+    omega /= embed_dim / 2.0
+    omega = 1.0 / 10000**omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = torch.einsum("m,d->md", pos, omega)  # (M, D/2), outer product
+
+    emb_sin = torch.sin(out)  # (M, D/2)
+    emb_cos = torch.cos(out)  # (M, D/2)
+
+    emb = torch.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    
+    return emb
+
 class MHCABlock(torch.nn.Module):
 
     def __init__(
@@ -100,6 +133,7 @@ class AutoRegressiveModel(torch.nn.Module):
                  num_heads: int = 4, 
                  n_blocks: int = 8,
                  n_tokens: int = 8,
+                 with_pos_embeddings: bool = True,
                  discretized_space: int = 300,
         ):
 
@@ -123,6 +157,8 @@ class AutoRegressiveModel(torch.nn.Module):
         self.output_tokens = torch.nn.Linear(dim, discretized_space)
         torch.nn.init.normal_(self.output_tokens.weight, std=0.02)
         
+        self.with_pos_embeddings = with_pos_embeddings
+        
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
 
         features = self.backbone(x)
@@ -132,6 +168,17 @@ class AutoRegressiveModel(torch.nn.Module):
         features = self.proj_dino_features(features)
 
         tokens = self.tokens.unsqueeze(0).repeat(features.shape[0], 1, 1)
+        
+        if self.with_pos_embeddings:
+            grid_h, grid_w = torch.meshgrid(torch.arange(2), torch.arange(2), indexing='ij')
+            grid = torch.stack([grid_h.ravel(), grid_w.ravel()], axis=0)  # Shape: (2, H*W)
+            
+            pos_embeds = get_2d_sincos_pos_embed_from_grid(tokens.shape[-1], grid)
+            pos_embeds = pos_embeds.unsqueeze(1).repeat(1, 2, 1)
+            pos_embeds = einops.rearrange(pos_embeds, "h w c -> (h w) c")
+            pos_embeds = pos_embeds.to(tokens.device)
+            
+            tokens += pos_embeds.unsqueeze(0)
 
         for mhsa, mhca in zip(self.MHSA_blocks, self.MHCA_blocks):
             
